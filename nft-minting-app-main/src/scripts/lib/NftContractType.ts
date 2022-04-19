d8c8bc8b79f021d3871cbd1edfae93335eaf7b7e
Exzo Network <89941175+Exzo-Network@users.noreply.github.com>
2022-04-18 20:22:03 -0400

    Add files via upload

---
+++ nft-minting-app-main/src/scripts/lib/NftContractType.ts
@@ -0,0 +1,7 @@
+// The name below ("Exzonaut") should match the name of your Solidity contract.
+// It can be updated using the following command:
+// yarn rename-contract NEW_CONTRACT_NAME
+// Please DO NOT change it manually!
+import { Exzonaut as NftContractType } from '../../../../smart-contract/typechain/index';
+
+export default NftContractType;
