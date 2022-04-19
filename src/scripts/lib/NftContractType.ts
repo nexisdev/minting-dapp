7357ed5ee0f5f0e84d4bb85c7edbd958217f983e
Exzo Network <89941175+Exzo-Network@users.noreply.github.com>
2022-04-19 14:40:50 -0400

    Add files via upload

---
+++ src/scripts/lib/NftContractType.ts
@@ -0,0 +1,7 @@
+// The name below ("ExzonautsUnmasked") should match the name of your Solidity contract.
+// It can be updated using the following command:
+// yarn rename-contract NEW_CONTRACT_NAME
+// Please DO NOT change it manually!
+import { ExzonautsUnmasked as NftContractType } from '../../../../smart-contract/typechain/index';
+
+export default NftContractType;
